4fe03236e6f7a4fa8589df233fafacb124bd03df
Kartik Chinda <kartik.chinda@gmail.com>
2024-05-02 19:42:58 +0530

    See you on the other side.

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-8xl font-mono font-bold">Bonsoir, Elliot</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
